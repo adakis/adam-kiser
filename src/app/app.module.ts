c4423d3630414c6bf277c078a6a711b74d662d7e
bracketeer <brent.stradling@gmail.com>
2018-06-24 22:52:08 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ReelComponent } from './components/reel/reel.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'reel', component: ReelComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ReelComponent,
+    ContactComponent,
+    ResumeComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
